8c7bfbc844fe02125d9bab137870e35fb1ccaffc
Colin Sagan <careof@colinsagan.com>
2019-07-06 14:07:43 -0400

    Initial file structure with infrastructure files from matfin/mattfinucane.com

---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const polyfills = [
+  'core-js/fn/array/from',
+  'core-js/fn/array/for-each'
+];
+
+module.exports = {
+  entry: [
+    ...polyfills,
+    './assets/scripts/main.js',
+    './assets/scss/main.scss'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../css/[name].css'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './assets/favicons',
+        to: '../favicons'
+      },
+      {
+        from: './assets/svg',
+        to: '../svg'
+      }
+    ])
+  ]
+};
